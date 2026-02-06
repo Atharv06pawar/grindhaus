d287014c68d5385a5aea316846a95d85df238969
Your Name <your@email.com>
2026-02-06 15:54:23 +0530

    Fix unused Router import

--- grindhaus/src/App.js
+++ grindhaus/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Route, Routes } from "react-router-dom";
+import {Route, Routes } from "react-router-dom";
 import Header from "./components/header.js";
 import Footer from "./components/footer.js";
 import Home from "./components/home.js";
